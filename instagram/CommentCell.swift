de4bcb589855173e7080f4e1ac0337747a9500a0
ShivaniNanan <105828071+ShivaniNanan@users.noreply.github.com>
2022-10-16 20:54:21 -0400

    Add files via upload

---
+++ instagram/CommentCell.swift
@@ -0,0 +1,31 @@
+//
+//  CommentCell.swift
+//  instagram
+//
+//  Created by Shivani Nanan on 9/25/22.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
